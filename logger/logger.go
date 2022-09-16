92c3016f069c82f0d339d97c890a39593a9e5ffb
coc0a25 <cranemont@gmail.com>
2022-09-16 23:53:34 +0900

    feat: add zap logger wrapper

---
+++ logger/logger.go
@@ -0,0 +1,87 @@
+package logger
+
+import (
+	"log"
+
+	"github.com/cranemont/judge-manager/constants"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Mode int
+type Env string
+
+const (
+	File Mode = 1 + iota
+	Console
+)
+
+const (
+	Production  Env = "production"
+	Development Env = "development"
+)
+
+type Logger struct {
+	zap *zap.Logger
+}
+
+func NewLogger(mode Mode, env Env) *Logger {
+	var zapLogger *zap.Logger
+	var cfg zap.Config
+	var err error
+
+	if env == Production {
+		cfg = zap.NewProductionConfig()
+		setMode(&cfg, mode, constants.LOG_PATH_PROD).
+			EncoderConfig = zap.NewProductionEncoderConfig()
+		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		zapLogger, err = cfg.Build(zap.AddCallerSkip(1))
+		if err != nil {
+			log.Fatalf("can't initialize zap logger: %v", err)
+		}
+	} else {
+		cfg = zap.NewDevelopmentConfig()
+		setMode(&cfg, mode, constants.LOG_PATH_DEV).
+			EncoderConfig = zap.NewDevelopmentEncoderConfig()
+		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		// cfg.Encoding = "json"
+		zapLogger, err = cfg.Build(zap.AddCallerSkip(1))
+		if err != nil {
+			log.Fatalf("can't initialize zap logger: %v", err)
+		}
+	}
+	return &Logger{zap: zapLogger}
+}
+
+func setMode(cfg *zap.Config, mode Mode, logPath string) *zap.Config {
+	switch mode {
+	case Console:
+	case File:
+		cfg.OutputPaths = []string{logPath}
+	case File | Console:
+		cfg.OutputPaths = append(cfg.OutputPaths, logPath)
+	default:
+		log.Fatalf("invalid logger mode: %d", mode)
+	}
+	return cfg
+}
+
+func (l *Logger) Debug(msg string, fields ...zapcore.Field) {
+	defer l.zap.Sync()
+	l.zap.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...zapcore.Field) {
+	defer l.zap.Sync()
+	l.zap.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...zapcore.Field) {
+	defer l.zap.Sync()
+	l.zap.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...zapcore.Field) {
+	defer l.zap.Sync()
+	l.zap.Error(msg, fields...)
+}
