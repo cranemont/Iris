de6c960e315444e936ead36ea32c0bdc44134c9d
cranemont <cranemont@gmail.com>
2022-09-18 15:23:53 +0900

    chore: apply new rabbitmq client implementations

--- ingress/mq/ingress.go
+++ ingress/mq/ingress.go
@@ -1,7 +1,9 @@
 package mq
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/cranemont/judge-manager/ingress/mq/rabbitmq"
 	"github.com/cranemont/judge-manager/logger"
@@ -30,28 +32,22 @@ func NewIngress(
 }
 
 func (i *ingress) Activate() {
-	// goroutine 등록
-	// 여기서 자동으로 consume해서 시작됨(go controller.call)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+		i.consumer.CleanUp()
+		i.producer.CleanUp()
+	}()
 
 	channelName := "submission"
 	queueName := "submission-queue"
-	exchangeName := "submission-exchange"
-	exchangeType := "direct"
-	bindingKey := "submission"
 
-	err := i.consumer.ChannelOpen(channelName)
+	err := i.consumer.OpenChannel(channelName)
 	if err != nil {
 		panic(err)
 	}
-	err = i.consumer.ExchangeDeclare(channelName, exchangeName, exchangeType)
-	if err != nil {
-		panic(err)
-	}
-	err = i.consumer.QueueDeclare(channelName, queueName)
-	if err != nil {
-		panic(err)
-	}
-	err = i.consumer.QueueBind(channelName, queueName, bindingKey, exchangeName)
+	
+	err = i.producer.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
@@ -64,32 +60,26 @@ func (i *ingress) Activate() {
 	// [mq.ingress]     consume -> handle -> 														  produce
 	// [mq.controller]							| controller -> 	controller(result) -> |
 	// [handler]													  | handler -> |
-	i.consume(messageCh, i.Done)
-
+	// i.consume(messageCh, i.Done)
+	for message := range messageCh {
+		go i.handle(message, ctx)
+	}
 	// running until Consumer is done
-	<-i.Done
+	// <-i.Done
 
-	if err := i.consumer.CleanUp(); err != nil {
-		i.logging.Error(fmt.Sprintf("failed to clean up the consumer: %s", err))
-	}
+	// if err := i.consumer.CleanUp(); err != nil {
+	// 	i.logging.Error(fmt.Sprintf("failed to clean up the consumer: %s", err))
+	// }
 }
 
-func (i *ingress) consume(messages <-chan amqp.Delivery, done chan error) {
-	clean := func() {
-		done <- nil
-	}
-	defer clean()
-
-	for message := range messages {
-		go i.handle(message)
-	}
-}
-
-func (i *ingress) handle(message amqp.Delivery) {
+func (i *ingress) handle(message amqp.Delivery, ctx context.Context) {
 	result := i.controller.Call(Judge, message.Body)
 
-	i.producer.Publish(result)
+	if err := i.producer.Publish(result, ctx); err != nil {
+		i.logging.Error(fmt.Sprintf("failed to publish result: %s: %s", string(result), err))
+	}
+
 	if err := message.Ack(false); err != nil {
-		i.logging.Error(fmt.Sprintf("failed to ack message: %s", err))
+		i.logging.Error(fmt.Sprintf("failed to ack message: %s: %s", string(message.Body), err))
 	}
 }
