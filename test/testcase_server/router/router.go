8c37b29816d36fe8d3c66c0480c96ed8b0d2c074
cranemont <cranemont@gmail.com>
2023-01-03 22:45:16 +0900

    feat: router 구현
    http.ListenAndServer의 두번째 인자로 넘기는 http.Handler 인터페이스 구현체
    router.HandleFunc(method, pattern, handler func)로 내부 handler map에 등록하여 사용

---
+++ test/testcase_server/router/router.go
@@ -0,0 +1,59 @@
+package router
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	hm "github.com/cranemont/judge-manager/test/testcase_server/router/method"
+)
+
+type router struct {
+	handler map[string]map[string]http.HandlerFunc
+}
+
+func NewRouter() *router {
+	return &router{
+		handler: make(map[string]map[string]http.HandlerFunc),
+	}
+}
+
+func (r *router) HandleFunc(method hm.HttpMethoder, pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	m := method.HttpMethod().ToString()
+	if _, ok := r.handler[m]; !ok {
+		r.handler[m] = make(map[string]http.HandlerFunc)
+	}
+	r.handler[m][pattern] = handler
+}
+
+func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for pattern, handler := range r.handler[req.Method] {
+		if params, ok := match(pattern, req.URL.Path); ok {
+			ctx := context.WithValue(req.Context(), "params", params)
+			handler(w, req.WithContext(ctx))
+			return
+		}
+	}
+	http.NotFound(w, req)
+}
+
+func match(pattern, path string) (map[string]string, bool) {
+	if pattern == path {
+		return nil, true
+	}
+
+	patterns := strings.Split(pattern, "/")
+	paths := strings.Split(path, "/")
+	params := make(map[string]string)
+
+	for idx, p := range patterns {
+		switch {
+		case p == paths[idx]:
+		case len(p) > 1 && p[0] == ':':
+			params[p[1:]] = paths[idx]
+		default:
+			return nil, false
+		}
+	}
+	return params, true
+}
