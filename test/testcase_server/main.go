2252b1e3f0940814e79a4a13ca122bf8072af4a6
coc0a25 <cranemont@gmail.com>
2023-01-04 02:43:35 +0900

    chore: adapter 동작 방식 설명 추가

--- test/testcase_server/main.go
+++ test/testcase_server/main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/cranemont/judge-manager/test/testcase_server/handler"
+	"github.com/cranemont/judge-manager/test/testcase_server/middleware"
 	"github.com/cranemont/judge-manager/test/testcase_server/router"
 	"github.com/cranemont/judge-manager/test/testcase_server/router/method"
 )
 
-func testcaseHandler(w http.ResponseWriter, req *http.Request) {
-	p := req.URL.Path
-	fmt.Println(p)
-	params, ok := req.Context().Value("params").(map[string]string)
-	if ok {
-		for k, v := range params {
-			fmt.Println(k, v)
-		}
-	}
-	w.Write([]byte{'d', 'e', 'f'})
-}
-
 func main() {
 	r := router.NewRouter()
-	r.HandleFunc(method.GET, "/problem/:id/testcase", testcaseHandler)
+	r.Handle(method.GET, "/problem/:id/testcase",
+		middleware.Adapt(
+			http.HandlerFunc(handler.TestcaseHandler),
+			middleware.Example(1),
+			middleware.Example(2),
+			middleware.Example(3),
+		),
+	)
 	http.ListenAndServe(":30000", r)
 }
