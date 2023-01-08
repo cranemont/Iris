18a07856432f734f140d5efeba565f818dec7486
cranemont <cranemont@gmail.com>
2023-01-08 19:31:36 +0900

    feat: testcase handler 구현
    임시로 파일 경로 설정해서 mock폴더에서 가져오도록 설정함
    환경변수로 설정할 수 있도록 수정 필요

--- mock/server/handler/testcase.go
+++ mock/server/handler/testcase.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/cranemont/judge-manager/mock/server/handler/response"
 )
@@ -15,6 +17,16 @@ func NewTestcaseHandler(r response.Responser) *testcase {
 }
 
 func (t *testcase) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// params, _ := req.Context().Value("params").(map[string]string)
-	t.r.Ok(w, response.Data{Message: "ok", Result: "dfsef"}, http.StatusOK)
+	params, _ := req.Context().Value("params").(map[string]string)
+
+	// FIXME: 경로 환경변수로 설정 가능하도록 수정
+	// FIXME: 테스트 가능한 구조로 분리
+	path, _ := filepath.Abs("../testcase")
+	data, err := os.ReadFile(path + "/" + params["id"] + ".json")
+	if err != nil {
+		t.r.Error(w, "failed to read testcase file", http.StatusNotFound)
+		return
+	}
+
+	t.r.Ok(w, response.Data{Message: "ok", Result: data}, http.StatusOK)
 }
