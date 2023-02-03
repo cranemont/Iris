dbb51cb45274d948ca79b95cbbba09943eda4a05
cranemont <cranemont@gmail.com>
2023-02-03 19:38:39 +0900

    refactor(testcase-server): 명시적인 이름으로 수정
    - SetContentType -> SetJsonContentType
    - 파일명도 json-content-type으로 수정

--- tests/testcase-server/middleware/json-content-type.go
+++ tests/testcase-server/middleware/json-content-type.go
@@ -2,7 +2,7 @@ package middleware
 
 import "net/http"
 
-func SetContentType() Adapter {
+func SetJsonContentType() Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
