892e87d368b02c3e966072957e82fec0f6a60654
coc0a25 <cranemont@gmail.com>
2023-02-21 16:33:54 +0900

    chore: devcontainer 설정 업데이트
    - 이미지 빌드해서 사용하도록 compose파일 수정
    - rabbitmq setup 스크립트에서 환경변수 사용

--- scripts/rabbitmq-setup.sh
+++ scripts/rabbitmq-setup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
+set -ex
 
 while ! nc -z "$RABBITMQ_HOST" "$RABBITMQ_PORT"; do sleep 3; done
->&2 echo "rabbitmq is up - server running..."
+echo "rabbitmq is up - server running..."
 
 # install rabbitmqadmin
-wget http://rabbitmq:15672/cli/rabbitmqadmin
+wget http://$RABBITMQ_HOST:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 
 # Make an Exchange
-./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare exchange name="$JUDGE_EXCHANGE_NAME" type=direct
+./rabbitmqadmin -H "$RABBITMQ_HOST" -u "$RABBITMQ_DEFAULT_USER" -p $RABBITMQ_DEFAULT_PASS declare exchange name="$JUDGE_EXCHANGE_NAME" type=direct
 
 # Make queues
-./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare queue name="$JUDGE_RESULT_QUEUE_NAME" durable=true
-./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare queue name="$JUDGE_SUBMISSION_QUEUE_NAME" durable=true
+./rabbitmqadmin -H "$RABBITMQ_HOST" -u "$RABBITMQ_DEFAULT_USER" -p $RABBITMQ_DEFAULT_PASS declare queue name="$JUDGE_RESULT_QUEUE_NAME" durable=true
+./rabbitmqadmin -H "$RABBITMQ_HOST" -u "$RABBITMQ_DEFAULT_USER" -p $RABBITMQ_DEFAULT_PASS declare queue name="$JUDGE_SUBMISSION_QUEUE_NAME" durable=true
 
 # Make bindings
-./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare binding source="$JUDGE_EXCHANGE_NAME"\
+./rabbitmqadmin -H "$RABBITMQ_HOST" -u "$RABBITMQ_DEFAULT_USER" -p $RABBITMQ_DEFAULT_PASS declare binding source="$JUDGE_EXCHANGE_NAME"\
                                 destination_type=queue destination="$JUDGE_RESULT_QUEUE_NAME" routing_key="$JUDGE_RESULT_ROUTING_KEY"
-./rabbitmqadmin -H rabbitmq -u skku -p 1234 declare binding source="$JUDGE_EXCHANGE_NAME"\
+./rabbitmqadmin -H "$RABBITMQ_HOST" -u "$RABBITMQ_DEFAULT_USER" -p $RABBITMQ_DEFAULT_PASS declare binding source="$JUDGE_EXCHANGE_NAME"\
                                 destination_type=queue destination="$JUDGE_SUBMISSION_QUEUE_NAME" routing_key="$JUDGE_SUBMISSION_ROUTING_KEY"
 
 
